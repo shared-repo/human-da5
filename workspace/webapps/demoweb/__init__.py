591318e24aca567a821dec2ecb49afa6515580e3
instructor <instructor@human.com>
2025-04-08 09:51:50 +0900

    update code

--- workspace/webapps/demoweb/__init__.py
+++ workspace/webapps/demoweb/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import render_template
 
+from .views import main_view
+
 def create_app(): # Flask가 웹 애플리케이션을 시작할 때 자동으로 호출하는 함수
 
     app = Flask(__name__) # web application 만들기
 
-    @app.route("/")
-    def index():
-        return render_template('index.html')
+    # @app.route("/")
+    # def index():
+    #     return render_template('index.html')
+
+    app.register_blueprint(main_view.main_bp)
 
     return app
